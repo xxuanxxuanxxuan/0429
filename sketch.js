5064249c1b376d0d531d954efcdde08872114fa4
xxuanxxuanxxuan <150638477+xxuanxxuanxxuan@users.noreply.github.com>
2024-04-29 11:53:26 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,12 +16,15 @@ function setup() {
   radioElement.position(width/2-300,20)
   radioElement.option("方塊")
   radioElement.option("圓圈")
+  radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.option("文字")
   radioElement.style("color","#fff")
-  //radioElement.style("font-size","30")
+  radioElement.style("font-size","30px")
 }
 
 function draw() {
-  background(0);
+  background(220);
   noStroke()
   span = 5+map(mouseX,0,width,0,20)
   push()
@@ -31,16 +34,38 @@ function draw() {
     for (var y= 0;x<captureGraphics.height;y=y+span){
       var pixel = captureGraphics.get(x,y)
       fill(pixel)
-      if(radioElement.value()=="方塊"){
+      if(radioElement.value()=="方塊" || radioElement.value()==""){
         rect(x,y,span)
       }
 
       if(radioElement.value()=="圓圈"){
-        rect(x,y,span)
-      rect(x,y,10)
-      //ellipse(x,y,span)
+        ellipse(x,y,span)
+      }
+        if(radioElement.value()=="亮度"){
+          bk = (pixel[0]+pixel[1]+pixel[2])/3
+          fill(bk)
+          ellipse(x,y,span) //span代表圓圈的直徑
+          ellipse(x,y,span*map(bk,0,255,0,1))
+        } 
+       if(radioElement.value()=="紅底"){
+        colorMode(HSB)
+        fill(pixel[0],80,80)
+        rectMode(CENTER)
+        rect(x,y,span*0.6)
+        fill(0)
+        ellipse(0,0,10)
+       }
+       if(radioElement.value()=="文字"){
+        const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+        let txt = "一二三四五田雷電龕龘"
+        bk=(pixel[0]+pixel[1]+pixel[2])/3
+        let bkId = int(map(bk, 0, 255, 9, 0))
+        textSize (span)
+        textStyle (BOLD)
+        text(txt[bkId],x,y)
+       }
     }
   }
   pop()
   }
-}
+
