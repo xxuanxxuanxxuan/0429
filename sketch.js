d1d7ee0a101d33afbabf75606201b3af8792a0ca
xxuanxxuanxxuan <150638477+xxuanxxuanxxuan@users.noreply.github.com>
2024-04-29 10:57:17 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,12 +9,19 @@ function setup() {
   captureGraphics = captureGraphics(capture_width,capture_height)
   capture. translate (capture_width,0)
   capture. scale(-1,1)
-  // capture.hide()
+  capture.hide()
+  //選鈕的介面
+  var radioElement = createRadio("方塊");
+  radioElement.position(width/2-300,20)
+  radioElement.option("方塊")
+  radioElement.option("圓圈")
+  radioElement.style("font-size","30")
 }
 
 function draw() {
-  background(220);
+  background(0);
   noStroke()
+  span = 5+map(mouseX,0,width,0,20)
   push()
   translate(width/capture_width,height/capture_height) //把原點移到(width/2-160,height/2-120)
   captureGraphics.image(capture,0,0) //在(0,0)
@@ -22,8 +29,16 @@ function draw() {
     for (var y= 0;x<captureGraphics.height;y=y+span){
       var pixel = captureGraphics.get(x,y)
       fill(pixel)
+      if(radioElement.value()=="方塊"){
+        rect(x,y,span)
+      }
+
+      if(radioElement.value()=="圓圈"){
+        rect(x,y,span)
       rect(x,y,10)
+      //ellipse(x,y,span)
     }
   }
   pop()
+  }
 }
